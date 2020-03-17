ac35c20f59a4279e9e19ce984fcb394da832dc4f
Shaji Ahmed <ahmeds@alphaxds.com>
2020-03-17 12:03:21 +0500

    Fixed debug

--- covid/settings.py
+++ covid/settings.py
@@ -25,7 +25,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1','corona.nhsrc.gov.pk']
 
