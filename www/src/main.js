05873c384817de82b3b1a3e43493398f6c45eb3d
Shaji Ahmed <ahmeds@alphaxds.com>
2020-03-19 10:49:58 +0500

    Trying a new header method

--- www/src/main.js
+++ www/src/main.js
@@ -4,12 +4,12 @@ import Axios from 'axios'
 import './filters'
 
 Axios.defaults.baseURL = process.env.VUE_APP_SERVER
-Axios.interceptors.request.use(
-  (config) => {
-    config.headers['Authorization'] = `Token ${process.env.VUE_APP_TOKEN}` 
-    return config
-  }, (error) => { return Promise.reject(error) }
-)
+// Axios.interceptors.request.use(
+//   (config) => {
+//     config.headers['Authorization'] = `Token ${process.env.VUE_APP_TOKEN}` 
+//     return config
+//   }, (error) => { return Promise.reject(error) }
+// )
 Axios.defaults.xsrfCookieName = 'csrftoken'
 Axios.defaults.xsrfHeaderName = 'X-CSRFToken'
 
