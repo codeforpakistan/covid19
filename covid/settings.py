eed61db15dd713015d49bc1ffce934f19685487e
Shaji Ahmed <ahmeds@alphaxds.com>
2020-03-20 14:10:43 +0500

    Switching to covid subdomain

--- covid/settings.py
+++ covid/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','54.186.56.147']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
