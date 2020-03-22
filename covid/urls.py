9b040c9587fce00c3ea0fcd784ce00898d2f1065
Shaji Ahmed <ahmeds@alphaxds.com>
2020-03-22 19:25:32 +0500

    Fixed missing route for /api/

--- covid/urls.py
+++ covid/urls.py
@@ -15,12 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import routers
 from api import views
 
 # Wire up our API using automatic URL routing.
+router = routers.DefaultRouter()
+
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', views.index),
+    path('api/', include(router.urls)),
     path('api/summary', views.summary),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
